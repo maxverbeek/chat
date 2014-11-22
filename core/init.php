19b4a3bc80811105eeff2e8a1da96a925a9ad88e
Max Verbeek <m4xv3rb33k@gmail.com>
2014-11-22 13:56:07 +0100

    Thought about chat functionality some more

--- core/init.php
+++ core/init.php
@@ -3,5 +3,6 @@
 session_start();
 
 $_SESSION['secret'] = 'asdf';
+$_SESSION['id'] = 34;
 
 $db = new PDO("mysql:host=127.0.0.1;dbname=chat", 'root', '');
--- index.php
+++ index.php
@@ -2,15 +2,23 @@
 
 require_once 'core/init.php';
 
+// the ID of the last message the client has received
+$id = isset($_GET['id']) ? (int) $_GET['id'] : 30;
+
+
+// fetch from this ID, up to the last message, but limit by 30
 $query = $db->prepare("
 	SELECT messages.id AS message_id, messages.message, users.id AS user_id, users.nickname
 	FROM messages
 	INNER JOIN users
 	ON messages.sender_id = users.id
-	WHERE users.secret = 'asdf'
+	WHERE users.secret = :secret OR messages.intended = :id
 	ORDER BY messages.id DESC
 	LIMIT 30
 ");
+
+$query->bindParam(':secret', $_SESSION['secret']);
+$query->bindParam(':id', $_SESSION['id']);
 
 $query->execute();
 
